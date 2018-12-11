1733c6cdc2c499138e9216f47b243c384ed4d834
Bob Weichler <bobmasaki@hotmail.com>
2018-12-11 09:30:15 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import Home from './components/Home/Home';
+
+// ========================================
+ReactDOM.render(<Home />, document.getElementById('root'));
